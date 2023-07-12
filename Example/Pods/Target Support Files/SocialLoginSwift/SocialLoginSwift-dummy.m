8d2899635d84d774391a563ee6084e06414e7757
Umang <umangmamtora4@gmail.com>
2023-07-12 22:02:46 +0530

    init

---
+++ Example/Pods/Target Support Files/SocialLoginSwift/SocialLoginSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SocialLoginSwift : NSObject
+@end
+@implementation PodsDummy_SocialLoginSwift
+@end
---
+++ Example/SocialLoginSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SocialLoginSwift
+//
+//  Created by Umang on 07/12/2023.
+//  Copyright (c) 2023 Umang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
